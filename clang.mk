19287627a2f35f5894d946e306bd65a181c9dd88
Amandeep Grewal <me@amandeep.ca>
2013-11-19 01:20:22 -0500

    Add experimental support for clang.

---
+++ clang.mk
@@ -0,0 +1,8 @@
+include Makefile
+
+# This makefile is for use if clang is installed on your machine.
+# Performance is noted to improve by approximately 40%.
+# Use this file at your own peril.
+
+CC = clang++
+CXX = clang++
